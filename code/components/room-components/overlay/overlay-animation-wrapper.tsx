398247dbefbbdbc34308712546e1f899432d396b
Agustin Jacobsen <agustinaj@ext.inditex.com>
2025-03-17 09:31:17 +0100

    Overlay animations (#58)
    * feat: toolbars
    
    * feat: multiuse overlay
    
    * chore: update dependencies
    
    * feat: added delay and new blur filter
    
    * feat: better UI managment for library panels
    
    ---------
    
    Co-authored-by: Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>

---
+++ code/components/room-components/overlay/overlay-animation-wrapper.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import { rightElementVariants } from "./variants";
+import { SelectionInformation } from "../selection-information";
+
+function OverlayAnimationWrapper({
+  children,
+  panelId,
+}: {
+  children: React.ReactNode;
+  panelId: string;
+}) {
+  return (
+    <motion.div
+      key={panelId}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      variants={rightElementVariants}
+      className="absolute top-[calc(50px+16px)] right-2 bottom-[calc(50px+16px)] flex flex-col gap-5 justify-center items-center"
+    >
+      <div className="w-[320px] p-0 h-full bg-white border border-zinc-200 shadow-xs flex justify-start items-center gap-3 overflow-hidden">
+        <div className="w-full h-full overflow-auto custom-scrollbar !px-0">
+          {children}
+          <SelectionInformation />
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default OverlayAnimationWrapper;
