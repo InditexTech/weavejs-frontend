7064ed2b3ac92654979cc5741885ffab0700cbdb
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-06-12 18:36:03 +0200

    chore: update dependencies

--- code/components/room-components/hooks/use-get-websockets-provider.ts
+++ code/components/room-components/hooks/use-get-websockets-provider.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { useCollaborationRoom } from "@/store/store";
 import { WeaveUser } from "@inditextech/weave-types";
 import { WeaveStoreWebsockets } from "@inditextech/weave-store-websockets/client";
