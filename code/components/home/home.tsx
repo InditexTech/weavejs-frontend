2b614f430a7e4054b24454bcc7acd6d31b4096a3
Jesus Manuel Piñeiro Cid <60609194+jesusmpc@users.noreply.github.com>
2025-06-17 11:28:47 +0200

    Update logo (#103)
    * chore: initial commit
    
    * feat: update logo

--- code/components/home/home.tsx
+++ code/components/home/home.tsx
@@ -28,17 +28,17 @@ export const Home = () => {
         >
           <div className="max-w-[520px] w-full flex flex-col items-center justify-between gap-0">
             <div className="w-full flex justify-between items-center gap-2 md:left-8 md:top-8 bg-background p-8 py-6 border border-[#c9c9c9]">
-              <Logo kind="small" />
+              <Logo kind="landscape" variant="no-text" />
               <motion.div
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.2 }}
                 className="flex flex-col items-end justify-center"
               >
-                <h1 className="text-3xl font-inter font-bold text-foreground uppercase">
+                <h1 className="text-2xl font-inter font-bold text-foreground uppercase">
                   WHITEBOARD
                 </h1>
-                <h2 className="text-2xl font-inter font-light text-muted-foreground uppercase">
+                <h2 className="text-1xl font-inter font-light text-muted-foreground uppercase">
                   SHOWCASE
                 </h2>
               </motion.div>
