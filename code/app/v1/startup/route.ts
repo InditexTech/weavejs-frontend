6b3d30591906275b22406ae4a3bdb3864571b080
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-07-07 16:26:29 +0200

    chore: support for health-checks

---
+++ code/app/v1/startup/route.ts
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
+export async function GET() {
+  return Response.json(
+    { status: "OK" },
+    {
+      status: 200,
+    }
+  );
+}
