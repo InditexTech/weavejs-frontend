1611d2941f59423fd031fa2829ac1e6e9be27d9f
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-09-01 15:49:39 +0200

    chore: fix reuse

--- code/components/room-components/hooks/types.ts
+++ code/components/room-components/hooks/types.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { WeaveUser } from "@inditextech/weave-types";
 
 export type ThreadStatus = "pending" | "resolved";
