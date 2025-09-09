728bb319e846fb30202536259ec4e8f0caa593ae
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-09-09 09:09:50 +0200

    chore: on AI, select model to try nano-banana and on the future others

--- code/api/types.ts
+++ code/api/types.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-export type Model =
+export type ImageModel =
   | "openai/gpt-image-1"
   | "gemini/gemini-2.5-flash-image-preview";
 export type ImageQuality = "low" | "medium" | "high";
