248b643f980cc7ebd0b632f0068097a011521b52
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-11-17 17:06:49 +0100

    chore: missing REUSE header

--- code/components/nodes/image-template/types.ts
+++ code/components/nodes/image-template/types.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { IMAGE_TEMPLATE_FIT } from "./constants";
 
 export type ImageTemplateFitKeys = keyof typeof IMAGE_TEMPLATE_FIT;
